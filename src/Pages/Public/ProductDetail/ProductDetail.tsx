3bdd554c94d3a4a2a898dd80a83fbddc41ad4850
phamducbinhh <phamducbinh1712000@gmail.com>
2023-12-05 17:06:39 +0700

    Update Done Call API

--- src/Pages/Public/ProductDetail/ProductDetail.tsx
+++ src/Pages/Public/ProductDetail/ProductDetail.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router'
+import { useAppDispatch } from '~/Redux/Hooks'
+import { fetchListProductDetails } from '~/Redux/Product/Product.Slice'
 
 const ProductDetail: React.FC = () => {
+  const route = useParams()
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchListProductDetails({ id: Number(route.id) }))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return <div>details</div>
 }
 
