9271430a62c4a07b640e6b7005c867a1ed5ef14f
phamducbinhh <phamducbinh1712000@gmail.com>
2023-12-05 15:56:28 +0700

    Update Product Pages

--- src/Pages/Public/Product/Product.tsx
+++ src/Pages/Public/Product/Product.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import FeatureTitle from '~/Components/FeatureTitle'
+import ShopArea from '~/Components/ShopArea'
+import { useAppDispatch, useAppSelector } from '~/Redux/Hooks'
+import { fetchListProduct } from '~/Redux/Product/Product.Slice'
 
 const Product: React.FC = () => {
-  return <div></div>
+  const dispatch = useAppDispatch()
+  const items = useAppSelector<any[]>((state) => state.product.listProduct)
+  const isLoading = useAppSelector<any>((state) => state.product.isLoading)
+  const settings = {
+    gridView: 4
+  }
+
+  useEffect(() => {
+    dispatch(fetchListProduct())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return (
+    <>
+      <FeatureTitle title='Our Shop' page='Shop' />
+      <ShopArea data={items} settings={settings} isLoading={isLoading} />
+    </>
+  )
 }
 
 export default Product
